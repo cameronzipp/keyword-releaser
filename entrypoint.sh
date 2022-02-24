667ad53c51672a16381f9a87846f762461281085
Cameron Zipp <78177750+cameronzipp@users.noreply.github.com>
2022-02-24 14:17:00 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,32 +21,23 @@ if jq '.commits[].message, .head_commit.message' < $EVENT_PATH | grep -i -q "$*"
 then
     # do something
     VERSION=$(date +%F.%s)
-    
+
     DATA="$(printf '{"tag_name":"v%s",' $VERSION)"
     DATA="${DATA} $(printf '"target_commitish":"master",')"
     DATA="${DATA} $(printf '"name":"v%s",' $VERSION)"
     DATA="${DATA} $(printf '"body":"Automated release based on keyword: %s",' "$*")"
     DATA="${DATA} $(printf '"draft":false, "prerelease":false}')"
-    
+
     URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/releases?access_token=${GITHUB_TOKEN}"
-    
+
     if [[ "${LOCAL_TEST}" == *"true"* ]];
     then
         echo "## [TESTING] Keyword was found but no release was created."
     else
         echo $DATA | http POST $URL | jq .
     fi
-
 # otherwise
 else
     # exit gracefully
     echo "Nothing to process."
 fi
-    
-    
-    
-    
-    
-    
-    
-    
